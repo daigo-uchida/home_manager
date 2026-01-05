1f81bc8c0a19fb38eb835fa61a47e54ea3e68560
productD5 <daigo.densiaddress@gmail.com>
2026-01-05 22:17:45 +0900

    fix :本番用設定

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -134,8 +134,10 @@ CORS_ALLOW_ALL_ORIGINS = True #add corsheaders　# すべてのオリジンを�
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
